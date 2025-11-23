a08e2b85bb791c40a3a12b451dbf5b1186b75e0a
Adrian Banks <adrian.banks@red-gate.com>
2025-11-23 21:54:58 +0000

    Add a new project for using no framework

---
+++ NoFramework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Parking.Domain;
+
+namespace Parking.NoFramework
+{
+    internal static class Program
+    {
+        internal static async Task Main()
+        {
+            var data = await DataFetcher.FetchData(SourceData.Url);
+            var carParks = CarParkParser.Parse(data);
+            var bestMatch = BestMatchCalculator.CalculateBestMatch(carParks);
+            var output = CarParkOutputFormatter.Format(bestMatch);
+            Console.WriteLine(output);
+        }
+    }
+}
