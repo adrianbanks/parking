fbdac4051b1f4574648ab2ddc65159b2c853fa16
Adrian Banks <adrian.banks@red-gate.com>
2025-11-24 21:31:27 +0000

    Add a fake version of the data loading

--- Parking.Domain/DataFetcher.cs
+++ Parking.Domain/DataFetcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
@@ -6,7 +7,17 @@ namespace Parking.Domain;
 
 public static class DataFetcher
 {
+    // set to false to fetch real data from the URL
+    private const string UseFakeData = "true";
+
     public static async Task<string> FetchData(string url)
+    {
+        return bool.Parse(UseFakeData)
+            ? FakeData()
+            : await FetchFromUrl(url);
+    }
+
+    private static async Task<string> FetchFromUrl(string url)
     {
         using var httpClient = new HttpClient();
 
@@ -16,5 +27,57 @@ public static class DataFetcher
 
         var response = await httpClient.PostAsync(url, body);
         return await response.Content.ReadAsStringAsync();
+    }
+
+    private static string FakeData()
+    {
+        var graftonEast = CreateRandomFullness(780);
+        var graftonWest = CreateRandomFullness(255);
+        var grandArcade = CreateRandomFullness(900);
+        var parkStreet = CreateRandomFullness(175);
+        var queenAnne = CreateRandomFullness(515);
+
+        return $$"""
+                  [
+                    {
+                      "name": "Grafton East",
+                      "totalSpaces": {{graftonEast.TotalSpaces}},
+                      "vacantSpaces": {{graftonEast.VacantSpaces}},
+                      "occupiedPercentage": {{graftonEast.OccupiedPercentage}}
+                    },
+                    {
+                      "name": "Grafton West",
+                      "totalSpaces": {{graftonWest.TotalSpaces}},
+                      "vacantSpaces": {{graftonWest.VacantSpaces}},
+                      "occupiedPercentage": {{graftonWest.OccupiedPercentage}}
+                    },
+                    {
+                      "name": "Grand Arcade",
+                      "totalSpaces": {{grandArcade.TotalSpaces}},
+                      "vacantSpaces": {{grandArcade.VacantSpaces}},
+                      "occupiedPercentage": {{grandArcade.OccupiedPercentage}}
+                    },
+                    {
+                      "name": "Park Street",
+                      "totalSpaces": {{parkStreet.TotalSpaces}},
+                      "vacantSpaces": {{parkStreet.VacantSpaces}},
+                      "occupiedPercentage": {{parkStreet.OccupiedPercentage}}
+                    },
+                    {
+                      "name": "Queen Anne",
+                      "totalSpaces": {{queenAnne.TotalSpaces}},
+                      "vacantSpaces": {{queenAnne.VacantSpaces}},
+                      "occupiedPercentage": {{queenAnne.OccupiedPercentage}}
+                    }
+                  ]
+                  """;
+    }
+
+    private static (int TotalSpaces, int VacantSpaces, int OccupiedPercentage) CreateRandomFullness(int totalSpaces)
+    {
+        var random = new Random();
+        var vacantSpaces = totalSpaces - random.Next(1, totalSpaces);
+        var percentageFull = Convert.ToInt32((double)(totalSpaces - vacantSpaces) / totalSpaces * 100);
+        return (totalSpaces, vacantSpaces, percentageFull);
     }
 }
